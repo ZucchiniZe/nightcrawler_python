775f6a2bdafbd301d53bcbda03b8b86a06f7043d
Alex B <me@alexb.io>
2016-05-31 12:04:13 -0700

    fix all the 404s

--- listing/urls.py
+++ listing/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^comics/$', views.AllTitleView.as_view(), name='listing'),
     url(r'^synced/$', views.SyncedView.as_view(), name='synced'),
     url(r'^comic/(?P<pk>[0-9]+)/$', views.ComicView.as_view(), name='comic'),
+    url(r'^title/(?P<pk>[0-9]+)/$', views.ComicView.as_view(), name='title'),
     url(r'^issue/(?P<pk>[0-9]+)/$', views.IssueView.as_view(), name='issue'),
     url(r'^creator/(?P<pk>[0-9]+)/$', views.CreatorView.as_view(), name='creator'),
     url(r'^refresh/comics/$', views.refresh_comics, name='refresh_comics'),
